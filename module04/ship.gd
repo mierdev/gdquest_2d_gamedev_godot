4faf6636cc84369e1b3450356a13cfcc15f1314f
mierdev <smurfmier@gmail.com>
2025-12-26 15:40:09 +0100

    Finished lesson 5

--- module04/ship.gd
+++ module04/ship.gd
@@ -7,9 +7,11 @@ var normal_speed := 600.0
 
 var max_speed := normal_speed
 var velocity := Vector2(0, 0)
+var steering_factor := 10.0
 
 
 func _process(delta: float) -> void:
+	# calculate direction
 	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
@@ -17,12 +19,18 @@ func _process(delta: float) -> void:
 	if direction.length() > 1.0:
 		direction = direction.normalized()
 
+	# boost function
 	if Input.is_action_just_pressed("boost"):
 		max_speed = boost_speed
 		timer.start()
-
-	velocity = direction * max_speed
+	
+	# apply velocity
+	var desired_velocity := max_speed * direction
+	var steering_vector := desired_velocity - velocity
+	velocity += steering_vector *  steering_factor * delta
 	position += velocity * delta
+	
+	# rotate sprite
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
 
