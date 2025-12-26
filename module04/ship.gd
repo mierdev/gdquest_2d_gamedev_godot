1b802b5f786ab890f087eb3dca88e185d7063c03
mierdev <smurfmier@gmail.com>
2025-12-26 16:52:59 +0100

    Finished lesson 6

--- module04/ship.gd
+++ module04/ship.gd
@@ -2,10 +2,10 @@ extends Sprite2D
 
 @onready var timer: Timer = $Timer
 
-var boost_speed := 1200.0
-var normal_speed := 600.0
+@export var max_speed := 600.0
+@export var boost_speed := 1500.0
 
-var max_speed := normal_speed
+var normal_speed := max_speed
 var velocity := Vector2(0, 0)
 var steering_factor := 10.0
 
@@ -18,8 +18,12 @@ func _process(delta: float) -> void:
 
 	if direction.length() > 1.0:
 		direction = direction.normalized()
-
-	# boost function
+		
+	# sprite rotation
+	if direction.length() > 0.0:
+		rotation = velocity.angle()
+		
+	# boost mechanic
 	if Input.is_action_just_pressed("boost"):
 		max_speed = boost_speed
 		timer.start()
@@ -27,12 +31,8 @@ func _process(delta: float) -> void:
 	# apply velocity
 	var desired_velocity := max_speed * direction
 	var steering_vector := desired_velocity - velocity
-	velocity += steering_vector *  steering_factor * delta
+	velocity += steering_factor * steering_vector * delta
 	position += velocity * delta
-	
-	# rotate sprite
-	if direction.length() > 0.0:
-		rotation = velocity.angle()
 
 
 func _on_timer_timeout() -> void:
